70474f20ab2c4b896594b737d23b0a542dc25fdb
Naved124 <nk7987513533@gmail.com>
2026-08-17 16:37:15 +0530

    fix(frontend): extract API URL to Vite environment variables

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/tasks';
+const API_URL = `${import.meta.env.VITE_API_URL}/tasks`;
 
 export const fetchTasks = async (status, sortBy) => {
   let url = API_URL;
